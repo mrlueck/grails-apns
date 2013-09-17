b9d0df9874fc3267c8d035b262e218270a07c7a5
mrlueck <Matthew.Lueck@gmail.com>
2013-09-17 15:43:26 -0600

    Update ApnsFactoryBean.groovy
    Add multiple certificate support

--- src/groovy/org/epseelon/grails/apns/ApnsFactoryBean.groovy
+++ src/groovy/org/epseelon/grails/apns/ApnsFactoryBean.groovy
@@ -16,43 +16,83 @@ class ApnsFactoryBean implements FactoryBean {
     String password
     boolean queued = false
     Environment environment = Environment.SANDBOX
-    boolean nonBlocking = false
-
+    
+	boolean nonBlocking = false
+	
+	Map certificates
+	
     Object getObject() {
-        def apnsService = APNS.newService()
-        if(certificateResourcePath){
-            apnsService = apnsService.withCert(this.getClass().getResourceAsStream(certificateResourcePath), password)
-        } else if(pathToCertificate){
-            apnsService = apnsService.withCert(pathToCertificate, password)
-        } else {
-            switch (environment){
-                case Environment.PRODUCTION: certificateResourcePath = "/apns-prod.p12"
-                    break
-                case Environment.SANDBOX: certificateResourcePath = "/apns-dev.p12"
-            }
-            apnsService = apnsService.withCert(this.getClass().getResourceAsStream(certificateResourcePath), password)
-        }
-		
-        switch(environment){
-            case Environment.PRODUCTION: 
-				apnsService = apnsService.withProductionDestination(); break;
-            case Environment.SANDBOX:
-            default: 
-				apnsService = apnsService.withSandboxDestination(); break;
-        }
-		
-		/*
-		 * Apns, nowadays, is just supporting Queued service. 
-		 * Was talking to the developer of the API, and he told me that furtermore
-		 * even the method asNonBlocking might be removed!
-		 */
-		  
-        apnsService = apnsService.asQueued()
-
-        return apnsService.build()
+		if ( certificates && certificates.size() > 0){
+			def apnsServices = [:]
+			certificates.each { key,value->
+				def apnsService = APNS.newService()
+				
+				if(value.certificateResourcePath){
+					
+					apnsService = apnsService.withCert(this.getClass().getResourceAsStream(value.certificateResourcePath), value.password)
+					
+				}else if(value.pathToCertificate){
+					
+					apnsService = apnsService.withCert(value.pathToCertificate, value.password)
+					
+				} 
+				
+				switch(environment){
+					case Environment.PRODUCTION:
+						apnsService = apnsService.withProductionDestination(); break;
+					case Environment.SANDBOX:
+					default:
+						apnsService = apnsService.withSandboxDestination(); break;
+				}
+				  
+				apnsService = apnsService.asQueued()
+				apnsServices[key] = apnsService.build()
+			}
+			
+			return apnsServices
+			
+		}else{
+	        def apnsService = APNS.newService()
+	        if(certificateResourcePath){
+	            apnsService = apnsService.withCert(this.getClass().getResourceAsStream(certificateResourcePath), password)
+	        } else if(pathToCertificate){
+	            apnsService = apnsService.withCert(pathToCertificate, password)
+	        } else {
+	            switch (environment){
+	                case Environment.PRODUCTION: certificateResourcePath = "/apns-prod.p12"
+	                    break
+	                case Environment.SANDBOX: certificateResourcePath = "/apns-dev.p12"
+	            }
+	            apnsService = apnsService.withCert(this.getClass().getResourceAsStream(certificateResourcePath), password)
+	        }
+			
+	        switch(environment){
+	            case Environment.PRODUCTION: 
+					apnsService = apnsService.withProductionDestination(); break;
+	            case Environment.SANDBOX:
+	            default: 
+					apnsService = apnsService.withSandboxDestination(); break;
+	        }
+			
+			/*
+			 * Apns, nowadays, is just supporting Queued service. 
+			 * Was talking to the developer of the API, and he told me that furtermore
+			 * even the method asNonBlocking might be removed!
+			 */
+			  
+	        apnsService = apnsService.asQueued()
+	
+	        return apnsService.build()
+		}
     }
 
-    Class getObjectType() { ApnsService }
+    Class getObjectType() { 
+		if ( certificates && certificates.size() > 0){
+			return ApnsService
+		}else{
+			return Map
+		} 
+    }
 
     boolean isSingleton() { true }
 }
